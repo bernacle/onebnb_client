9003b76d7414889efa43c962251fcbe65c4cfd0b
Bruno Simplicio <brunoraphael89@gmail.com>
2017-01-08 14:44:45 -0200

    Create app module structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+ 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
+ 
+import { routing } from './app.routing';
+ 
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +17,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
