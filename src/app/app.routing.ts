abc201dc0c9a543cf1f4aa56a09e784e92a4d5bc
Bruno Simplicio <brunoraphael89@gmail.com>
2017-01-22 17:24:49 -0200

    Install and Configure Angular2Token, add Login and Register Components

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,16 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule }   from '@angular/router';
- 
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './users/register/register.component';
+import { LoginComponent } from './users/login/login.component';
+import { Angular2TokenService } from 'angular2-token';
  
  
 // Cria nossas Rotas
 const appRoutes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent }
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'register', component: RegisterComponent }, 
+  { path: 'login', component: LoginComponent }
 ];
  
 // Exporte a constante routing para importarmos ela no arquivo app.module.ts
