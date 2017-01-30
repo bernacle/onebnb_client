0280904f8ab2597625d8525b29e1d3964bc0ca60
Bruno Simplicio <brunoraphael89@gmail.com>
2017-01-30 21:19:30 -0200

    Create Components Search, Filter and Pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,45 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { DropdownModule, AlertModule, ButtonsModule, DatepickerModule, CollapseModule, ModalModule } from 'ng2-bootstrap/ng2-bootstrap'; 
+import { DropdownModule, AlertModule, ButtonsModule, DatepickerModule, CollapseModule, ModalModule, PaginationModule} from "ng2-bootstrap/ng2-bootstrap";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
  
 import { routing } from './app.routing';
-
+ 
 import { Property } from './shared/property';
 import { PropertiesService } from './shared/properties.service';
 import { Angular2TokenService, A2tUiModule} from 'angular2-token';
-import { LoginComponent } from './users/login/login.component';
 import { RegisterComponent } from './users/register/register.component';
+import { LoginComponent } from './users/login/login.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
-import { ResetPasswordComponent } from './users/reset-password/reset-password.component';
-import { UpdatePasswordComponent } from './users/update-password/update-password.component';
 import { ResultsComponent } from './results/results.component';
+import { SearchComponent } from './shared/search/search.component';
+import { FilterComponent } from './shared/filter/filter.component';
+import { PaginationComponent } from './shared/pagination/pagination.component';
  
  
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    HeaderComponent,
-    FooterComponent,
-    UserEditComponent,
-    ResetPasswordComponent,
-    UpdatePasswordComponent,
-    ResultsComponent
+	AppComponent,
+	HomeComponent,
+	RegisterComponent,
+	LoginComponent,
+	HeaderComponent,
+	FooterComponent,
+	UserEditComponent,
+	ResultsComponent,
+	SearchComponent,
+	FilterComponent,
+	PaginationComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    routing,
-    DropdownModule.forRoot(),
-    CollapseModule
+	BrowserModule,
+	FormsModule,
+	HttpModule,
+	routing,
+	AlertModule.forRoot(),
+	DropdownModule.forRoot(),
+	CollapseModule,
+	DatepickerModule.forRoot(),
+	PaginationModule.forRoot()
   ],
   providers: [PropertiesService, Angular2TokenService],
   bootstrap: [AppComponent]
