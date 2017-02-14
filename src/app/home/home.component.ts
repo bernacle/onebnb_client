56f10622d830b8c38ead8e0a7a37948687161a77
Bruno Simplicio <brunoraphael89@gmail.com>
2017-02-14 18:10:42 -0200

    Create comment box and user resume box

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
   constructor(private PropertiesService: PropertiesService) { }
 
   ngOnInit(){
-    this.search('Brazil');
+
   }
 
   search(query) {
--- src/app/property/property-details/property-details.component.ts
+++ src/app/property/property-details/property-details.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { PropertiesService } from '../../shared/properties.service';
 import { Property } from '../../shared/property';
 
+
 @Component({
   selector: 'app-property-details',
   templateUrl: './property-details.component.html',
@@ -13,7 +14,14 @@ export class PropertyDetailsComponent implements OnInit {
   constructor(private PropertiesService: PropertiesService, private route: ActivatedRoute, private router: Router) { }
 
   private property: Property;
+  private address: any = [];
+  private details: any = [];
+  private facilities: any = [];
   private photos: any = [];
+  private comments: any = [];
+  private profile: any = [];
+  private lat: number = 0;
+  private lng: number = 0;
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -22,10 +30,16 @@ export class PropertyDetailsComponent implements OnInit {
           data => {
             this.property = data;
             this.photos = data.property.photos;
+            this.comments = data.property.comments;
+            this.profile =  data.property.user;
+            this.details =  data.property;
+            this.address =  data.property.address;
+            this.facilities = data.property.facility;
+            this.lat = +data.property.address.latitude;
+            this.lng = +data.property.address.longitude;
           },
           err => {this.router.navigateByUrl('/');}
         );
     });
   }
-
 }
