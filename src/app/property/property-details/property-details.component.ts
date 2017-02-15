7de86e53890e5f72b3c4a475bd62b0e11ead9479
Bruno Simplicio <brunoraphael89@gmail.com>
2017-02-15 21:09:21 -0200

    Include map box in property details and create component send message

--- src/app/property/property-details/property-details.component.ts
+++ src/app/property/property-details/property-details.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { PropertiesService } from '../../shared/properties.service';
 import { Property } from '../../shared/property';
-
-
+ 
+ 
 @Component({
   selector: 'app-property-details',
   templateUrl: './property-details.component.html',
   styleUrls: ['./property-details.component.scss']
 })
 export class PropertyDetailsComponent implements OnInit {
-
+ 
   constructor(private PropertiesService: PropertiesService, private route: ActivatedRoute, private router: Router) { }
-
+ 
   private property: Property;
   private address: any = [];
   private details: any = [];
@@ -22,7 +22,7 @@ export class PropertyDetailsComponent implements OnInit {
   private profile: any = [];
   private lat: number = 0;
   private lng: number = 0;
-
+ 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.PropertiesService.getProperty(params['id'])
